0f9efbeffd8ce88c4e91b43d9e8728c6b37e417c
rasuru <rasuru@protonmail.com>
2023-05-02 14:36:36 +0600

    Fix Back button not working on home page

--- src/ui/setup/router.tsx
+++ src/ui/setup/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, redirect } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import { MainBoardPage } from 'src/task.feature/main-board.feature/ui/MainBoardPage';
 import { TaskPage } from 'src/task.feature/viewed-task.feature/ui/TaskPage';
 import { AppLayout } from '../AppLayout';
@@ -14,33 +14,41 @@ export const AppRoute = {
 
 export const router = createBrowserRouter([
 	{
-		path: '/',
-		ErrorBoundary: ErrorPage,
-		loader: () => redirect(AppRoute.MainBoard),
-	},
-	{
 		element: <AppLayout />,
-		ErrorBoundary: ErrorPage,
 		children: [
 			{
-				path: AppRoute.MainBoard,
+				path: '/',
+				ErrorBoundary: ErrorPage,
 				loader: async () => {
 					const { adapters } = await import('./adapters');
-
 					adapters.viewMainBoardController.run();
+
+					history.replaceState({}, document.title, AppRoute.MainBoard);
 					return null;
 				},
 				Component: MainBoardPage,
-			},
-			{
-				path: AppRoute.Task.URLTemplate,
-				loader: async ({ params }) => {
-					const { adapters } = await import('./adapters');
+				children: [
+					{
+						path: AppRoute.MainBoard,
+						loader: async () => {
+							const { adapters } = await import('./adapters');
 
-					adapters.viewTaskController.run(params.taskID!);
-					return null;
-				},
-				Component: TaskPage,
+							adapters.viewMainBoardController.run();
+							return null;
+						},
+						Component: MainBoardPage,
+					},
+					{
+						path: AppRoute.Task.URLTemplate,
+						loader: async ({ params }) => {
+							const { adapters } = await import('./adapters');
+
+							adapters.viewTaskController.run(params.taskID!);
+							return null;
+						},
+						Component: TaskPage,
+					},
+				],
 			},
 		],
 	},
